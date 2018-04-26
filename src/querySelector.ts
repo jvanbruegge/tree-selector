1ff57a49636f63e4a3ed30f2d3265fc0c064cfd4
Jan van Brügge <supermanitu@gmail.com>
2018-04-26 12:18:25 +0200

    Add support for direct child combinator

--- src/querySelector.ts
+++ src/querySelector.ts
@@ -1,4 +1,4 @@
-import { Selector, parseSelector } from './selectorParser';
+import { Selector, parseSelector, Combinator } from './selectorParser';
 import { Options } from './types';
 import { createMatches } from './matches';
 
@@ -7,30 +7,45 @@ export function createQuerySelector<T>(
 ): (sel: string | Selector, node: T) => T[] {
     const matches = createMatches(options);
 
-    function findSubtree(selector: Selector, node: T): T[] {
-        if (selector.nextSelector !== undefined) {
-            throw new Error('Should not happen');
-        }
-
+    function find(selector: Selector, depth: number, node: T): T[] {
         const matched = matches(selector, node) ? [node] : [];
-        const childMatched = options
-            .children(node)
-            .filter(c => typeof c !== 'string')
-            .map(c => findSubtree(selector, c as T))
-            .reduce((acc, curr) => acc.concat(curr), []);
+        if(depth > 0) {
+            const childMatched = options
+                .children(node)
+                .filter(c => typeof c !== 'string')
+                .map(c => find(selector, depth - 1, c as T))
+                .reduce((acc, curr) => acc.concat(curr), []);
 
-        return matched.concat(childMatched);
+            return matched.concat(childMatched);
+        }
+        return matched;
     }
 
     return function querySelector(selector: string | Selector, node: T): T[] {
         const sel =
             typeof selector === 'object' ? selector : parseSelector(selector);
 
+        let results: T[] = [node];
+
         let currentSelector = sel;
-        let tail = sel.nextSelector;
+        let currentCombinator: Combinator = 'subtree';
+        let tail = undefined;
+        do {
+            tail = currentSelector.nextSelector;
+            currentSelector.nextSelector = undefined;
 
-        currentSelector.nextSelector = undefined;
+            const depth = currentCombinator === 'subtree' ? Infinity : 1;
 
-        return findSubtree(currentSelector, node);
+            results = results
+                .map(n => find(currentSelector, depth, n))
+                .reduce((acc, curr) => acc.concat(curr), []);
+
+            if (tail) {
+                currentSelector = tail[1];
+                currentCombinator = tail[0];
+            }
+        } while (tail !== undefined);
+
+        return results;
     };
 }
--- test/querySelector.ts
+++ test/querySelector.ts
@@ -22,4 +22,51 @@ describe('querySelector', () => {
 
         assert.deepStrictEqual(querySelector(selector, vtree), expected);
     });
+
+    it('should find node with direct child selector', () => {
+        const selector = 'div#id > .class';
+        const vtree = {
+            tag: 'div',
+            children: [
+                { tag: 'span', id: 'id' },
+                { tag: 'div', id: 'id', children: [
+                    { tag: 'div', className: 'class right', children: [
+                        { tag: 'div', className: 'class wrong' }
+                    ] }
+                ] }
+            ]
+        };
+
+        const result = querySelector(selector, vtree);
+
+        assert.strictEqual(result.length, 1);
+        assert.strictEqual(typeof result[0], 'object');
+        assert.strictEqual(result[0].tag, 'div');
+        assert.strictEqual(result[0].className, 'class right');
+    });
+
+    it('should find multiple node if having same class', () => {
+        const selector = 'div#id .class';
+        const vtree = {
+            tag: 'div',
+            children: [
+                { tag: 'span', id: 'id' },
+                { tag: 'div', id: 'id', children: [
+                    { tag: 'div', className: 'class class1', children: [
+                        { tag: 'div', className: 'class class2' }
+                    ] }
+                ] }
+            ]
+        };
+
+        const result = querySelector(selector, vtree);
+
+        assert.strictEqual(result.length, 2);
+        assert.strictEqual(typeof result[0], 'object');
+        assert.strictEqual(typeof result[1], 'object');
+        assert.strictEqual(result[0].tag, 'div');
+        assert.strictEqual(result[0].className, 'class class1');
+        assert.strictEqual(result[1].tag, 'div');
+        assert.strictEqual(result[1].className, 'class class2');
+    });
 });
