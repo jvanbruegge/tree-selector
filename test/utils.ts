ec1233dab702c6ecdf11183875b13ff129b243b3
Jan van Brügge <supermanitu@gmail.com>
2018-09-07 11:24:31 +0300

    Fix permutation algorithm to run tests (#3)

--- test/utils.ts
+++ test/utils.ts
@@ -1,11 +1,5 @@
 import { VNode } from './vnode';
 
-export function permutations<T>(arr: T[]): T[][] {
-    return arr
-        .map(e => permutations(arr.slice(1)).map(arr => [e].concat(arr)))
-        .reduce((acc, curr) => acc.concat(curr), []);
-}
-
 export function addParents(node: VNode): VNode {
     node.children = !node.children
         ? undefined
